47d80ac30aa9fb4283c587479d21e1fc1b9a73eb
Khaschuluu Munkhbayar <khaschuluu.m@gmail.com>
2021-10-25 13:24:41 +0800

    mint to different address

--- contracts/StorepayCoin.sol
+++ contracts/StorepayCoin.sol
@@ -1,11 +1,11 @@
 // contracts/StorepayCoin.sol
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract StorepayCoin is ERC20Burnable {
-    constructor() ERC20("Storepay Coin", "SPC") {
-        _mint(msg.sender, 600000000000*10**18);
+    constructor(address _mintTo) ERC20("Storepay Coin", "SPC") {
+        _mint(_mintTo, 600000000000*10**18);
     }
 }
